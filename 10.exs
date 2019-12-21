5f98d3791070864d4de85bd8e3a574e0915c6c2f
Tom Rothe <info@tomrothe.de>
2019-12-21 10:03:16 +0100

    fix read logic in 10

--- 10.exs
+++ 10.exs
@@ -16,15 +16,27 @@
 defmodule AstroidMap do
   @moduledoc "Parameter such as star, from and to are coordinates of the form {x,y}."
   
+  def read(filename) do
+    input = File.read!(filename)
+      |> String.split
+      |> Enum.map(&String.codepoints/1)
+
+    width = length(Enum.at(input, 0))
+    height = length(input)
+
+    for y <- 0..(height-1),  x <- 0..(width-1), Enum.at(Enum.at(input, y), x) == "#", do: {x, y} # this is very imperative thinking, could be improved
+  end
+
   @doc "is star hidden behind the line segment between from and to?"
   def occludes?(from, to, star)
   def occludes?(p, p, _), do: false
-  def occludes?(from, to, star), do: on_line?(from, star, to)
+  def occludes?(_, p, p), do: false
+  def occludes?(from, to, star), do: on_segment?(from, star, to)
 
   @doc "is the given point on the line segment between from and to?"
-  def on_line?(from, to, point)
-  def on_line?(p, p, _), do: false
-  def on_line?(from, to, point) do
+  def on_segment?(from, to, point)
+  def on_segment?(p, p, _), do: false
+  def on_segment?(from, to, point) do
     # using this approach: https://stackoverflow.com/a/11908158/4007237
     # see this explanation for more info why this works: http://www.sunshine2k.de/coding/java/PointOnLine/PointOnLine.html
     [{fx,fy}, {tx,ty}, {px,py}] = [from, to, point]
@@ -42,23 +54,42 @@ defmodule AstroidMap do
     end    
   end
 
-  def unoccluded_stars_from(star, stars) do
-    
+  @doc "can source see the other star? (is there no star between source and other?)"
+  def can_see?(source, other, stars) do
+    stars
+    |> Enum.all?(fn star -> !occludes?(source, star, other) end)
+  end
+
+  def visible_star_count(source, stars) do
+    Enum.count(stars, fn star -> can_see?(source, star, stars) end) - 1
   end
 end
 
-ExUnit.start()
+# ExUnit.start()
 
-defmodule MapTest do
-  use ExUnit.Case
-  # we can not use doctest here, because I want to work in a single file
-  test "occludes?" do
-    AstroidMap.occludes?({1,1}, {2,2}, {3,3}) |> assert
-    AstroidMap.occludes?({0,0}, {0,1}, {0,4}) |> assert
-    AstroidMap.occludes?({1,1}, {3,1}, {5,1}) |> assert
-    AstroidMap.occludes?({0,0}, {3,0}, {1,0}) |> refute
-    AstroidMap.occludes?({2,2}, {5,2}, {1,2}) |> refute
-    AstroidMap.occludes?({1,1}, {1,1}, {0,0}) |> refute
-  end
-end
+# defmodule MapTest do
+#   use ExUnit.Case
+#   import AstroidMap
+#   # we can not use doctest here, because I want to work in a single file
+#   test "occludes?" do
+#     occludes?({1,1}, {2,2}, {3,3}) |> assert
+#     occludes?({0,0}, {0,1}, {0,4}) |> assert
+#     occludes?({1,1}, {3,1}, {5,1}) |> assert
+#     occludes?({0,0}, {3,0}, {1,0}) |> refute
+#     occludes?({2,2}, {5,2}, {1,2}) |> refute
+#     occludes?({1,1}, {1,1}, {0,0}) |> refute
+#   end
+
+#   test "can_see?" do
+#     can_see?({1,1}, {3,3}, [{1,1}]) |> assert
+#     can_see?({1,1}, {3,3}, [{1,1}, {2,2}]) |> refute
+#   end
+# end
+
+stars = AstroidMap.read("10.txt")
+
+stars
+  |> Enum.map(fn star -> AstroidMap.visible_star_count(star, stars) end)
+  |> Enum.max
+  |> IO.inspect
 
